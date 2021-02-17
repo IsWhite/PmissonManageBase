ea84c1639c69a5cd4826a7902eda37e45ad53a31
IsWhite <2240641509@qq.com>
2021-02-17 19:21:13 +0800

    暂时注掉的方法

--- server/src/main/java/com/debug/pmp/server/config/MybatisPlusConfig.java
+++ server/src/main/java/com/debug/pmp/server/config/MybatisPlusConfig.java
@@ -1,7 +1,7 @@
 package com.debug.pmp.server.config;
 
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+//import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+//import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +17,8 @@ public class MybatisPlusConfig {
     }
 
     //防sql注入攻击
-    @Bean
-    public ISqlInjector sqlInjector() {
-        return new LogicSqlInjector();
-    }
+//    @Bean
+//    public ISqlInjector sqlInjector() {
+//        return new LogicSqlInjector();
+//    }
 }
